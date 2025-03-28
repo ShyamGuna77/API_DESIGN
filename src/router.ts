edac52bdad427316c2b688ed41627bfca3307a7b
Shyam <gshyamprasad2001@gmail.com>
2025-03-28 08:15:44 +0530

    added routes logic

--- src/router.ts
+++ src/router.ts
@@ -3,27 +3,32 @@
 import express from 'express'
 const router = express.Router()
 import { body, validationResult } from "express-validator";
+import { handleInputErrors } from './modules/middleware';
+import { updateUpdate } from './handlers/update';
 
 router.get('/product' ,(req,res) => {
     res.json({message:"Hello Nigga"})
 })
-router.get("/product/:id", () => {});
-router.post("/product/:id", body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    res.status(400);
-    res.json({ errors: errors.array() });
-  }
+router.get("/product/:id", () => {});
+router.post("/product/:id", body("name").isString(), handleInputErrors,(req,res)=>{
+
 });
-router.post("/product", () => {});
+router.post("/product",body('name').isString(),handleInputErrors, );
 router.delete("/product/:id", () => {});
 
 //Update Routes
 
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
+  body("version").optional(),
+  updateUpdate
+);
 router.post("/update", () => {});
 router.delete("/update/:id", () => {});
 
@@ -31,8 +36,19 @@ router.delete("/update/:id", () => {});
 
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "/updatepoint",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router
